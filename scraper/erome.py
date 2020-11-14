b0923588929a6f40a4a9528b78f2793ff1e9c666
Amenly <60410674+Amenly@users.noreply.github.com>
2020-11-14 17:00:18 -0600

    Update erome.py

--- scraper/erome.py
+++ scraper/erome.py
@@ -1,268 +1,203 @@
 import os
 import sys
 import json
-import requests
 import concurrent.futures
+import re
+from collections import Counter
 
+import requests
 from bs4 import BeautifulSoup
+from blessings import Terminal
+from tqdm import tqdm
 
 
-def text_replacement(text):
-    dictionary = {'.': '_', ':': '_', '\\': '_', '/': '_'}
-    for k, v in dictionary.items():
-        text = text.replace(k, v)
-    return text
-
-
-def check_path(path):
-    if not os.path.isdir(path):
-        return False
-
-
-def dir_img(path):
-    dir_image = path + "/Images"
-    if not os.path.isdir(dir_image):
-        os.mkdir(dir_image)
-    return dir_image
-
-
-def dir_vid(path):
-    dir_video = path + "/Videos"
-    if not os.path.isdir(dir_video):
-        os.mkdir(dir_video)
-    return dir_video
-
-
-def dir_parent(url):
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        title = soup.find("h1")
-        actual_title = title.text
-        actual_title = text_replacement(actual_title)
-        dir_parent = main_path + "/" + actual_title
-        if not os.path.isdir(dir_parent):
-            os.mkdir(dir_parent)
-        return dir_parent, actual_title
-
-
-def dir_parent_profile(url):
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        username = soup.find("div", {"class": "col-sm-5 user-info username"})
-        actual_title = username.text.strip().replace("\n FOLLOW", "")
-        actual_title = text_replacement(actual_title)
-        dir_parent = main_path + "/" + actual_title
-        if not os.path.isdir(dir_parent):
-            os.mkdir(dir_parent)
-        return dir_parent
-
-
-def dir_parent_profile_album(url, path):
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        title = soup.find("h1")
-        actual_title = title.text
-        actual_title = text_replacement(actual_title)
-        dir_parent = path + "/" + actual_title
-        if not os.path.isdir(dir_parent):
-            os.mkdir(dir_parent)
-        return dir_parent, actual_title
-
-
-def dl_img(url, filename, index):
-    try:
-        with requests.get(url) as connection:
-            with open(os.path.join(dir_img(parent_path), filename), "wb") as f:
-                for chunk in connection.iter_content(chunk_size=50000000):
-                    f.write(chunk)
-        print(f"└── {filename} (✓) {index}/{length}")
-    except:
-        print(f"└── {filename} (✗) {index}/{length}")
-
-
-def dl_vid(url, filename, index):
-    try:
-        with requests.get(url) as connection:
-            with open(os.path.join(dir_vid(parent_path), filename), "wb") as f:
-                for chunk in connection.iter_content(chunk_size=50000000):
-                    f.write(chunk)
-        print(f"└── {filename} (✓) {index}/{length}")
-    except:
-        print(f"└── {filename} (✗) {index}/{length}")
-
-
-def scr_img(url, album_title):
-    img_urls = []
-    filenames = []
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        images = soup.findAll("img", {"class": "img-back lasyload"})
-        if len(images) == 0:
-            pass
-        else:
-            for image in images:
-                img_url = image["data-src"]
-                img_urls.append(img_url)
-                filename = img_url.split("/")[-1]
-                filenames.append(filename)
-            global length
-            length = len(img_urls)
-            img_range = range(1, length + 1)
-            print(f"{album_title}")
-            print(f"\nDownloading {length} images...")
-            print("Images")
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                executor.map(dl_img, img_urls, filenames, img_range)
-
-
-def scr_vid(url, album_title):
-    comparison = []
-    vid_urls = []
-    filenames = []
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        videos = soup.findAll("source")
-        if len(videos) == 0:
-            pass
-        else:
-            for v in videos:
-                if v not in comparison:
-                    comparison.append(v)
-            for video in comparison:
-                vid_url = video["src"]
-                vid_urls.append(vid_url)
-                filename = vid_url.split("/")[-1]
-                filenames.append(filename)
-            global length
-            length = len(comparison)
-            vid_range = range(1, length + 1)
-            print(f"\nDownloading {length} videos...")
-            print("Videos")
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                executor.map(dl_vid, vid_urls, filenames, vid_range)
-
-
-def profile_pages(url):
-    num = 0
-    while 1:
-        num = num + 1
-        changing_url = url.format(num)
-        with requests.get(changing_url) as connection:
-            content = connection.content
-            soup = BeautifulSoup(content, "html.parser")
-            links = soup.findAll("a", {"class": "album-link"})
-            if links == []:
-                break
-            else:
-                scrape_profile_page(changing_url)
-
-
-def start_session(url, user_agent, email, password):
-    payload = {"remember": "on"}
-    payload["email"] = email
-    payload["password"] = password
-    headers = {}
-    headers["user-agent"] = user_agent
-    login_url = "https://www.erome.com/user/login"
-    with requests.Session() as s:
-        r = s.get(login_url, headers=headers)
-        content = r.content
-        soup = BeautifulSoup(content, "html.parser")
-        token = soup.find("input", {"name": "_token"})["value"]
-        payload["_token"] = token
-        headers["referer"] = login_url
-        p = s.post(login_url, data=payload, headers=headers)
-        r = s.get(url)
-        num = 0
-        alt_url = url + "?page={}"
-        while 1:
-            num += 1
-            changing_url = alt_url.format(num)
-            r = s.get(changing_url)
-            content = r.content
-            soup = BeautifulSoup(content, "html.parser")
-            links = soup.findAll("a", {"class": "album-link"})
-            if links == []:
-                break
-            else:
-                parent_url = changing_url
-                r = s.get(changing_url)
-                content = r.content
-                soup = BeautifulSoup(content, "html.parser")
-                links = soup.findAll("a", {"class": "album-link"})
-                for link in links:
-                    album_link = link["href"]
-                    link_process_profile(album_link, parent_url)
-
-
-def scrape_profile_page(url):
-    parent_url = url
-    with requests.get(url) as connection:
-        content = connection.content
-        soup = BeautifulSoup(content, "html.parser")
-        links = soup.findAll("a", {"class": "album-link"})
-        for link in links:
-            album_link = link["href"]
-            link_process_profile(album_link, parent_url)
-
-
-def link_process_profile(url, p_url):
-    profile_path = dir_parent_profile(p_url)
-    global parent_path
-    parent_path, actual_title = dir_parent_profile_album(url, profile_path)
-    print(f"\nScraping the '{actual_title}' album...")
-    scr_img(url, actual_title)
-    scr_vid(url, actual_title)
-
-
-def link_process(url):
-    global parent_path
-    parent_path, actual_title = dir_parent(url)
-    print(f"\nScraping the '{actual_title}' album...")
-    scr_img(url, actual_title)
-    scr_vid(url, actual_title)
-
-
-def menu():
-    x = -1
-    x = int(input(
-        "0 = scrape an album | 1 = scrape a profile | 2 = scrape a private profile\n> "))
-    if x == 0:
-        url_user = input("Enter the album link here\n> ")
-        link_process(url_user)
-        print("Done")
-    elif x == 1:
-        profile_link = input("Enter the profile link here\n> ")
-        profile_link = profile_link + "?page={}"
-        profile_pages(profile_link)
-        print("Done")
-    elif x == 2:
-        private_profile_link = input("Enter the profile link here\n> ")
-        with open("config.json") as config:
-            settings = json.load(config)["settings"]
-            email = settings["email"]
-            password = settings["password"]
-            user_agent = settings["user_agent"]
-        start_session(private_profile_link, user_agent, email, password)
+class Directory:
+    album_directory = ""
+    profile_directory = ""
 
 
 def main():
-    if check_path(main_path) is False:
-        print("Are you sure your path is correct? Please recheck your config.")
+    x = -1
+    x = int(input(
+        "0 = scrape an album | 1 = scrape a profile | 2 = scrape a private profile\n>>> "))
+    if x == 0:
+        url = input("Enter the album link here\n>>> ")
+        link_process(url)
+        print(term.magenta("Done"))
+    elif x == 1:
+        url = input("Enter the profile link here\n>>> ")
+        scrape_profile(url)
+        print(term.magenta("Done"))
+    elif x == 2:
+        url = input("Enter the profile link here\n>>> ")
+        scrape_private_profile(url)
+        print(term.magenta("Done"))
+
+
+def link_process(url):
+    make_dir(url)
+    scrape_images(url)
+    scrape_videos(url)
+
+
+def make_dir(url):
+    with requests.Session() as s:
+        r = s.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "lxml")
+    title = soup.find("h1")
+    album_title = text_replacement(title.text)
+    if dir_profile := instance.profile_directory:
+        if not os.path.isdir(dir_profile):
+            os.mkdir(dir_profile)
+        dir_album = dir_profile + "/" + album_title
+        if not os.path.isdir(dir_album):
+            os.mkdir(dir_album)
     else:
-        menu()
+        dir_album = folder + "/" + album_title
+        if not os.path.isdir(dir_album):
+            os.mkdir(dir_album)
+    instance.album_directory = dir_album
+    print(f"\nScraping the '{term.bold(album_title)}' album...")
 
 
-os.chdir(sys.path[0])
-with open("config.json") as config:
-    settings = json.load(config)["settings"]
-    main_path = settings["folder_path"]
+def text_replacement(string):
+    pattern = re.compile(r'[.:/\\]')
+    new_string = pattern.sub('_', string)
+    return new_string
+
+
+def scrape_images(url):
+    with requests.Session() as s:
+        r = s.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "lxml")
+    images = soup.findAll("img", {"class": "img-back lasyload"})
+    if not images:
+        pass
+    else:
+        image_urls = [image["data-src"] for image in images]
+        len_images = len(image_urls)
+        print(term.bold(f"Found {len_images} images"))
+        with tqdm(total=len_images, desc="Downloading images") as bar:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = {executor.submit(
+                    download_image, image_url): image_url for image_url in image_urls}
+                for future in concurrent.futures.as_completed(futures):
+                    future.result
+                    bar.update(1)
+
+
+def download_image(url):
+    filename = url.split("/")[-1]
+    with requests.Session() as s:
+        r = s.get(url, headers=headers)
+    with open(os.path.join(create_images_dir(instance.album_directory), filename), "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            f.write(chunk)
+
+
+def create_images_dir(path):
+    images_dir = path + "/Images"
+    if not os.path.isdir(images_dir):
+        os.mkdir(images_dir)
+    return images_dir
+
+
+def scrape_videos(url):
+    with requests.Session() as s:
+        r = s.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "lxml")
+    videos = soup.findAll("source")
+    if not videos:
+        pass
+    else:
+        videos = list(Counter(videos))
+        video_urls = [video["src"] for video in videos]
+        len_videos = len(videos)
+        print(term.bold(f"Found {len_videos} videos"))
+        with tqdm(total=len_videos, desc="Downloading videos") as bar:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = {executor.submit(
+                    download_video, video_url): video_url for video_url in video_urls}
+                for future in concurrent.futures.as_completed(futures):
+                    future.result
+                    bar.update(1)
+
+
+def download_video(url):
+    filename = url.split("/")[-1]
+    with requests.Session() as s:
+        r = s.get(url)
+    with open(os.path.join(create_videos_dir(instance.album_directory), filename), "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            f.write(chunk)
+
+
+def create_videos_dir(path):
+    videos_dir = path + "/Videos"
+    if not os.path.isdir(videos_dir):
+        os.mkdir(videos_dir)
+    return videos_dir
+
+
+def scrape_profile(url):
+    page = 0
+    username = url.split("/")[-1]
+    instance.profile_directory = folder + "/" + username
+    url += "?page={}"
+    print(f"Found {term.magenta(username)}'s profile'")
+    while True:
+        page += 1
+        with requests.Session() as s:
+            r = s.get(url.format(page))
+        soup = BeautifulSoup(r.content, "lxml")
+        links = soup.findAll("a", {"class": "album-link"})
+        if not links:
+            break
+        else:
+            print(term.magenta(f"\nScraping albums on page {page}..."))
+            album_urls = [link["href"] for link in links]
+            for album_url in album_urls:
+                link_process(album_url)
+
+
+def scrape_private_profile(url):
+    login_url = login["referer"]
+    payload = login
+    page = 0
+    username = url.split("/")[-1]
+    instance.profile_directory = folder + "/" + username
+    url += "?page={}"
+    print(f"Found {term.magenta(username)}'s profile'")
+    with requests.Session() as s:
+        r = s.get(login_url, headers=headers)
+        r.close()
+        soup = BeautifulSoup(r.content, "lxml")
+        token = soup.find("input", {"name": "_token"})["value"]
+        payload["_token"] = token
+        r = s.post(login_url, data=payload, headers=headers)
+        while True:
+            page += 1
+            r = s.get(url.format(page))
+            r.close()
+            soup = BeautifulSoup(r.content, "lxml")
+            links = soup.findAll("a", {"class": "album-link"})
+            if not links:
+                break
+            else:
+                print(term.magenta(f"\nScraping albums on page {page}..."))
+                album_urls = [link["href"] for link in links]
+                for album_url in album_urls:
+                    link_process(album_url)
 
 
 if __name__ == "__main__":
+    config_file = sys.path[0] + "/config.json"
+    with open(config_file) as f:
+        config = json.load(f)["config"]
+    headers = config["headers"]
+    if not (folder := config["settings"]["folder_path"]):
+        folder = os.getcwd()
+    login = config["login"]
+    instance = Directory()
+    term = Terminal()
     main()
