8f2075b81aa14a91d628e08ac8f86dc72dd2b25c
Amenly <60410674+Amenly@users.noreply.github.com>
2020-12-21 15:52:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 CLASSIFIERS = [
-    "Development Status :: 5 - Production/Stable",
-    'Programming Language :: Python :: 3',
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     'Operating System :: OS Independent',
 ]
